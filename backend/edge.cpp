ae857adfb7340cb25c9d097423aee5c65ba180b8
David Lee <dle84@uclive.ac.nz>
2025-11-23 22:12:05 +1300

    Edge class and header.

---
+++ backend/edge.cpp
@@ -0,0 +1,80 @@
+#include "edge.h"
+#include <cmath>
+#include <sstream>
+
+//Contructor implementations
+Edge::Edge()
+    : nodes{nullptr, nullptr}, weight(INFINITY) {}
+
+Edge::Edge(Node* nodeA, Node* nodeB)
+    : nodes{nodeA, nodeB}, weight(getEuclideanDistance(nodeA, nodeB)) {
+    if (!isValidEdge()) {
+        throw std::invalid_argument("Edge cannot connect a node to itself.");
+    }
+}
+
+Edge::Edge(Node* nodeA, Node* nodeB, double weight)
+    : nodes{nodeA, nodeB}, weight(weight) {
+    if (!isValidEdge()) {
+        throw std::invalid_argument("Edge cannot connect a node to itself.");
+    }
+}
+
+//Setter implementations
+void Edge::setNodes(Node* nodeA, Node* nodeB) {
+
+    if (!nodeA || !nodeB || nodeA == nodeB) {
+        throw std::invalid_argument("Edge cannot connect a node to itself.");
+    }
+
+    this->nodes[0] = nodeA;
+    this->nodes[1] = nodeB;
+}
+
+void Edge::setWeight(double weight) {
+    this->weight = weight;
+}
+
+//Getter implementations
+const std::array<Node*, 2>& Edge::getNodes() const {
+    return this->nodes;
+}
+
+double Edge::getWeight() const {
+    return this->weight;
+}
+
+//Methods
+double Edge::getEuclideanDistance(Node* nodeA, Node* nodeB) {
+    double dx = nodeA->getLatitude() - nodeB->getLatitude();
+    double dy = nodeA->getLongitude() - nodeB->getLongitude();
+    return std::sqrt(dx*dx + dy*dy);
+}
+
+//Helper implementations
+std::string Edge::toString() const {
+    std::ostringstream oss;
+    oss << "Edge(";
+    oss << "nodeA=";
+
+    if (nodes[0]) oss << nodes[0]->getName();
+    else oss << "null";
+
+    oss << ", nodeB=";
+
+    if (nodes[1]) oss << nodes[1]->getName();
+    else oss << "null";
+
+    oss << ", w=" << weight << ")";
+
+    return oss.str();
+}
+Node* Edge::getOtherNode(Node* node) const {
+    if (node == nodes[0]) return nodes[1];
+    else if (node == nodes[1]) return nodes[0];
+    else return nullptr;
+}
+
+bool Edge::isValidEdge() const {
+    return nodes[0] && nodes[1] && nodes[0] != nodes[1];
+}
---
+++ backend/edge.h
@@ -0,0 +1,36 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include "Node.h"
+#include <array>
+#include <string>
+
+class Edge
+{
+private:
+    std::array<Node*, 2> nodes;
+    double weight;
+public:
+    //Getters
+    const std::array<Node*, 2>& getNodes() const;
+    double getWeight() const;
+
+    //Setters
+    void setNodes(Node* nodeA, Node* nodeB);
+    void setWeight(double weight);
+
+    //Methods
+    static double getEuclideanDistance(Node* nodeA, Node* nodeB);
+
+    //Helpers
+    std::string toString() const;
+    Node* getOtherNode(Node* node) const;
+    bool isValidEdge() const;
+
+    //Contructors
+    Edge();
+    Edge(Node* nodeA, Node* nodeB);
+    Edge(Node* nodeA, Node* nodeB, double weight);
+};
+
+#endif // EDGE_H
