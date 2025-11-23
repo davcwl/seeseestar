1bfd2ea063458693c3451a7a7200be5426f70173
David Lee <dle84@uclive.ac.nz>
2025-11-23 23:00:32 +1300

    Start of Graph class. Basic outline of header completed.

--- backend/graph.h
+++ backend/graph.h
@@ -2,10 +2,33 @@
 #define GRAPH_H
 
 #include "SeeSeeStar_global.h"
+#include <vector>
+#include "node.h"
+#include "edge.h"
 
 class SEESEESTAR_EXPORT Graph
 {
+private:
+    std::vector<Node*> nodes;
+    std::vector<Edge*> edges;
+
 public:
+
+    void addNode(Node* node);
+    void addEdge(Edge* edge);
+
+    void removeNode(Node* node);
+    void removeEdge(Edge* edge);
+
+    Node* findNodeById(int id) const;
+
+    std::string toString() const;
+    Edge* findEdge(Node* nodeA, Node* nodeB) const;
+
+    double getDistance(Node* nodeA, Node* nodeB) const;
+
+    std::vector<Node*> shortestPath(Node* start, Node* end) const;
+
     Graph();
 };
 
