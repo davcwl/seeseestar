421995544cea0962b8643ee6e2f16c65f724a1c9
David Lee <dle84@uclive.ac.nz>
2025-12-02 21:40:22 +1300

    Added additional C API for Node functions.

--- backend/api.cpp
+++ backend/api.cpp
@@ -3,43 +3,59 @@
 
 extern "C" {
 
-API Graph* CreateGraph() {
-    return new Graph();
-}
+    API Graph* CreateGraph() {
+        return new Graph();
+    }
 
-API void DestroyGraph(Graph* graph) {
-    delete graph;
-}
+    API void DestroyGraph(Graph* graph) {
+        delete graph;
+    }
 
-API void Graph_AddNode(Graph* graph, int id, const char* name, double latitude, double longitude) {
-    graph->addNode(new Node(id, name, latitude, longitude));
-}
+    API void Graph_AddNode(Graph* graph, int id, const char* name, double latitude, double longitude) {
+        graph->addNode(new Node(id, name, latitude, longitude));
+    }
 
-API void Graph_AddEdge(Graph* graph, int nodeA_ID, int nodeB_ID) {
-    Node* nodeA = graph->findNodeById(nodeA_ID);
-    Node* nodeB = graph->findNodeById(nodeB_ID);
-    graph->addEdge(new Edge(nodeA, nodeB));
-}
+    API void Graph_AddEdge(Graph* graph, int nodeA_ID, int nodeB_ID) {
+        Node* nodeA = graph->findNodeById(nodeA_ID);
+        Node* nodeB = graph->findNodeById(nodeB_ID);
+        graph->addEdge(new Edge(nodeA, nodeB));
+    }
 
-API void Graph_SetStart(Graph* graph, int nodeID) {
-    graph->setStartingNode(graph->findNodeById(nodeID));
-}
+    API void Graph_SetStart(Graph* graph, int nodeID) {
+        graph->setStartingNode(graph->findNodeById(nodeID));
+    }
 
-API void Graph_SetGoal(Graph* graph, int nodeID) {
-    graph->setGoalNode(graph->findNodeById(nodeID));
-}
+    API void Graph_SetGoal(Graph* graph, int nodeID) {
+        graph->setGoalNode(graph->findNodeById(nodeID));
+    }
 
-//Gets nodes of shortest path, then makes a C style array copy.
-API Node** Graph_CalculateShortestPath(Graph* graph, int* outLength) {
+    //Gets nodes of shortest path, then makes a C style array copy.
+    API Node** Graph_CalculateShortestPath(Graph* graph, int* outLength) {
 
-    auto nodesShortestPath = graph->calculateShortestPath();
-    *outLength = nodesShortestPath.size();
+        auto nodesShortestPath = graph->calculateShortestPath();
+        *outLength = nodesShortestPath.size();
 
-    Node** result = new Node*[nodesShortestPath.size()];
+        Node** result = new Node*[nodesShortestPath.size()];
 
-    for (int i = 0; i < nodesShortestPath.size(); ++i)
-        result[i] = nodesShortestPath[i];
+        for (int i = 0; i < nodesShortestPath.size(); ++i)
+            result[i] = nodesShortestPath[i];
 
-    return result;
-}
+        return result;
+    }
+
+    API int Node_GetID(Node* node) {
+        return node->getId();
+    }
+
+    API double Node_GetLatitude(Node* node) {
+        return node->getLatitude();
+    }
+
+    API double Node_GetLongitude(Node* node) {
+        return node->getLongitude();
+    }
+
+    API const char* Node_GetName(Node* node) {
+        return node->getName().c_str();
+    }
 }
--- backend/api.h
+++ backend/api.h
@@ -19,5 +19,10 @@ API void Graph_AddEdge(Graph* graph, int nodeA_ID, int nodeB_ID);
 API void Graph_SetStart(Graph* graph, int nodeID);
 API void Graph_SetGoal(Graph* graph, int nodeID);
 
+API int Node_GetID(Node* node);
+API double Node_GetLatitude(Node* node);
+API double Node_GetLongitude(Node* node);
+API const char* Node_GetName(Node* node);
+
 API Node** Graph_CalculateShortestPath(Graph* graph, int* outLength);
 }
