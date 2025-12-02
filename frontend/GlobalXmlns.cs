2c43b92c1c2d5147dd8109185bd3a52df8e94fd6
David Lee <dle84@uclive.ac.nz>
2025-12-02 21:38:57 +1300

    Created .NET MAUI project for frontend.

---
+++ frontend/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "frontend")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "frontend.Pages")]
